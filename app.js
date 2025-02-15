e1390016f39cac105ff0ce9bfad4f2093247291c
JavierPena5526 <javape5526@gmail.com>
2025-02-14 21:03:11 -0600

    Made a straw string

---
+++ app.js
@@ -0,0 +1,23 @@
+// const dice1 = 6;
+// const dice2 = 3;
+
+// if (dice1 === 6 || dice2 === 6) {
+//     console.log ("You rolled a double");
+// } else {
+//     console.log("you didn't");
+// }
+
+// const name = "Edwin";
+// const age = 24;
+
+// const combined = name + age;
+
+// console.log(typeof age);
+
+const straw1 = 10;
+
+if (straw1 >= 5) {
+    console.log(`You picked the long straw!`)
+} else {
+    console.log(`You picked the short straw.`)
+}
